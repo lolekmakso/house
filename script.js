797ca8866b0f4addef309c1d28ba5037a2cf1d73
lolekmakso <makstarasovv1@gmail.com>
2023-02-19 22:49:32 +0200

    house system

---
+++ script.js
@@ -0,0 +1,6 @@
+function navToggle() {
+    var headerMenu = document.getElementById("headerMenu");
+    var menuIcon = document.getElementById("menuIcon");
+    headerMenu.classList.toggle("_active");
+    menuIcon.classList.toggle("_active");
+  }
